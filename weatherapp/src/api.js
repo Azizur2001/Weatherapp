a896d2bade8ff1c112a776e05c921f1863c9b023
azizur rahman <azizurrahman@azizurs-air.mynetworksettings.com>
2023-06-19 18:46:59 -0400

    New project to push to Github

---
+++ weatherapp/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6452d3eb19msh019080b372c3459p1ad70bjsn4667663c6fb6",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8d8b85dc140ed34d5a38d42bcdc6744a";
